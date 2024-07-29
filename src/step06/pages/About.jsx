6a32b6054fed3bd7c22688d7cc4dde1cddb9f9b0
Jammomu <pridom03@naver.com>
2024-07-29 15:01:55 +0900

    step06 nav style

---
+++ src/step06/pages/About.jsx
@@ -0,0 +1,10 @@
+import './About.css';
+
+export function About(){
+  return (
+    <div className="about_container">
+      <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit eaque eveniet deleniti, fuga explicabo quos maxime dolorum eligendi facere corporis eum accusamus atque ipsa error natus exercitationem laudantium illum adipisci.</span>
+      <span>영화리뷰사이트, 2024</span>
+    </div>
+  );
+}
